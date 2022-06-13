3453c0defec7c34e7076c3e1025127cc76e194d7
unknown <narjessheikhali@gmail.com>
2022-06-13 21:22:08 +0430

    add edit

--- phonebook/src/component/Form.jsx
+++ phonebook/src/component/Form.jsx
@@ -19,9 +19,9 @@ export const Form = ({users, setUser, setUsers, user})=>{
     return(
 
         <>
-        {hide?
+        {hide && (
         <div style={{border:"1px solid gray"}}>
-        <svg onClick={()=>setHide(false)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+        <svg onClick={()=>setHide(!hide)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg>
         <form id="headFrm" onSubmit={handleAdd}>
@@ -45,7 +45,7 @@ value={user.phonenumber}/>
 <input  type={"submit"}/>
 </div>
 </form>
-</div> : null}
+</div> )}
         </>
     )
 }
